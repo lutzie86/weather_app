f43303aa36a5f949db661adbe02bc2ea62ccf19f
Christine Lutz <christinelutz33@yahoo.com>
2019-01-18 12:00:17 -0500

    weather by zipcode applicatiopn

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,29 @@
+class WelcomeController < ApplicationController
+  def test
+  	response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?zip=#{params[:zipcode]}&units=imperial&appid=#{ENV['openweather_api_key']}")
+  	@location = response['name']
+  	@temp = response['main']['temp']
+  	@weather_icon = response['weather'][0]['icon']
+  	@weather_words = response['weather'][0]['description'] 
+  	@cloudiness = response['clouds']['all']
+  	@windiness = response['wind']['speed']
+
+  end
+
+  def index
+  	#checks that location params are not empty before calling api
+  	if params[:zipcode] != '' && !params[:zipcode].nil?
+  		response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?zip=#{params[:zipcode]}&units=imperial&appid=#{ENV['openweather_api_key']}")
+  		@status = response['cod']
+  	#if no error returned fill instance variable with result of api call
+  		if @status != '404' && response['message'] != 'city not found' 
+  		@location = response['name']
+  		@temp = response['main']['temp']
+  		@weather_icon = response['weather'][0]['icon']
+  		@weather_words = response['weather'][0]['description'] 
+  		@cloudiness = response['clouds']['all']
+  		@windiness = response['wind']['speed']
+  		end
+  	end
+  end
+end
